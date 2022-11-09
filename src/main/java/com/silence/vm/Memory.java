5a10be1f528672006be66c2e69813bd38bade4ea
forsilence <3312034934@qq.com>
2022-11-09 10:42:48 +0800

    Memory read and write

--- src/main/java/com/silence/vm/Memory.java
+++ src/main/java/com/silence/vm/Memory.java
@@ -3,11 +3,11 @@ package com.silence.vm;
 public class Memory {
     public static final int MEMORY_MAX = 1 << 16;
     private static final short[] memory = new short[MEMORY_MAX];
-    public static short get(int ind){
+    public static short mem_read(int ind){
         return memory[ind];
     }
 
-    public static void set(int ind, short val){
+    public static void mem_write(int ind, short val){
         memory[ind] = val;
     }
 
