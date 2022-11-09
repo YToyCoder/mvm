6de7f5432901123fa2af59813fdc1c8043cc8369
forsilence <3312034934@qq.com>
2022-11-09 10:42:29 +0800

    change register type as int literal

--- src/main/java/com/silence/vm/Registers.java
+++ src/main/java/com/silence/vm/Registers.java
@@ -1,27 +1,16 @@
 package com.silence.vm;
 
 public class Registers {
-    public enum Register {
-        R_R0(0),
-        R_R1(1),
-        R_R2(2),
-        R_R3(3),
-        R_R4(4),
-        R_R5(5),
-        R_R6(6),
-        R_R7(7),
-        R_PC(8), /** program counter */
-        R_COND(9), /** condition register , condition calculation result*/
-        R_COUNT(10);
-        public int val(){
-            return val;
-        }
+    public static final int R_R0 = 0;
+    public static final int R_R1 = 1;
+    public static final int R_R2 = 2;
+    public static final int R_R3 = 3;
+    public static final int R_R4 = 4;
+    public static final int R_R5 = 5;
+    public static final int R_R6 = 6;
+    public static final int R_R7 = 7;
+    public static final int R_PC = 8;
+    public static final int R_COND = 9;
+    public static final int R_COUNT = 10;
 
-        private final int val;
-
-        Register(int _val){
-            this.val = _val;
-        }
-    }
-//    static
 }
